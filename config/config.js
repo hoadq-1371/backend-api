d78c6d855998c202468f7f4aa8452a8604a741a3
Hoa Doan Quang <doan.quang.hoa@sun-asterisk.com>
2019-06-18 14:39:37 +0700

    add default JWT_SECRET

--- config/config.js
+++ config/config.js
@@ -16,7 +16,7 @@ const envVarsSchema = Joi.object({
       then: Joi.boolean().default(true),
       otherwise: Joi.boolean().default(false)
     }),
-  JWT_SECRET: Joi.string().required().description('JWT Secret required to sign'),
+  JWT_SECRET: Joi.string().required().default('0a6b944d-d2fb-46fc-a85e-0295c986cd9f').description('JWT Secret required to sign'),
   MONGO_HOST: Joi.string().required().description('Mongo DB host url'),
   MONGO_PORT: Joi.number().default(27017)
 }).unknown().required();
